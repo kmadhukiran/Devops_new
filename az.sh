aa29c0bcceca742663264d7507a8afeea9a8d98d
kmadhukiran <58160350+kmadhukiran@users.noreply.github.com>
2019-12-12 13:56:10 +0530

    Update az.sh

--- az.sh
+++ az.sh
@@ -8,6 +8,7 @@ sudo -S <<< "Azure.123456@e" yum update -y
 sudo -S <<< "Azure.123456@e" yum install java-1.8.0-openjdk -y
 sudo -S <<< "Azure.123456@e" wget http://apachemirror.wuchna.com/tomcat/tomcat-9/v9.0.29/bin/apache-tomcat-9.0.29.tar.gz
 sudo -S <<< "Azure.123456@e" tar -xvzf apache-tomcat-9.0.29.tar.gz
-sudo -S <<< “Azure.123456@e” scp -r /var/root/.jenkins/workspace/Azure/hello-world-war-1.0.0.war azureuser@13.92.193.107:/home/azureuser/apache-tomcat-9.0.27/webapps -p Azure.123456@e 
+sudo -S <<< “Azure.123456@e” wget https://github.com/kmadhukiran/Devops_new/blob/master/sample.war
+sudo -S <<< “Azure.123456@e” mv sample.war /home/azureuser/apache-tomcat-9.0.29/webapps
 sudo -S <<< “Azure.123456@e” cd /home/azureuser/apache-tomcat-9.0.27/bin
 sudo -S <<< “Azure.123456@e” ./startup.sh

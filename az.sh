91754ede1a088da481630bbce1638646165ed9fe
kmadhukiran <58160350+kmadhukiran@users.noreply.github.com>
2019-12-12 14:15:52 +0530

    Update az.sh

--- az.sh
+++ az.sh
@@ -8,7 +8,7 @@ sudo -S <<< "Azure.123456@e" yum update -y
 sudo -S <<< "Azure.123456@e" yum install java-1.8.0-openjdk -y
 sudo -S <<< "Azure.123456@e" wget http://apachemirror.wuchna.com/tomcat/tomcat-9/v9.0.29/bin/apache-tomcat-9.0.29.tar.gz
 sudo -S <<< "Azure.123456@e" tar -xvzf apache-tomcat-9.0.29.tar.gz
-sudo -S <<< “Azure.123456@e” wget https://github.com/kmadhukiran/Devops_new/blob/master/sample.war
-sudo -S <<< “Azure.123456@e” mv sample.war /home/azureuser/apache-tomcat-9.0.29/webapps
+sudo -S <<< “Azure.123456@e” wget https://github.com/kmadhukiran/Devops_new/blob/master/calendar.war
+sudo -S <<< “Azure.123456@e” mv calendar.war /home/azureuser/apache-tomcat-9.0.29/webapps
 sudo -S <<< “Azure.123456@e” cd /home/azureuser/apache-tomcat-9.0.29/bin
-sudo -S <<< “Azure.123456@e” /home/azureuser/apache-tomcat-9.0.29/bin/./startup.sh
+sudo -S <<< “Azure.123456@e” /home/azureuser/apache-tomcat-9.0.29/bin/./startup.sh << 'ENDSSH'

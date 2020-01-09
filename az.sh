00ac0e90e091934151f066078f8090244f4ca822
kmadhukiran <58160350+kmadhukiran@users.noreply.github.com>
2020-01-09 12:15:12 +0530

    Update az.sh

--- az.sh
+++ az.sh
@@ -5,7 +5,8 @@ az vm open-port --port 80 --resource-group madhu09012020 --name task1
 az vm open-port --port 22 --resource-group madhu09012020 --name task1
 sshpass -p 'Azure.123456@e' ssh -t -t -o StrictHostKeyChecking=no azureuser@13.92.193.107 << 'ENDSSH'
 sudo -S <<< "Azure.123456@e" yum update -y
-
+sudo -S <<< "Azure.123456@e" yum install git* -y
+sudo -S <<< "Azure.123456@e" git clone https://github.com/kmadhukiran/Devops_new.git
 exit
 exit
 ENDSSH

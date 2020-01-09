c874adebda2a8e8f2c5fbf64590bbfd8e0cfb0c7
kmadhukiran <58160350+kmadhukiran@users.noreply.github.com>
2020-01-09 15:58:13 +0530

    Update az.sh

--- az.sh
+++ az.sh
@@ -7,8 +7,10 @@ ip=$(az vm show --resource-group madhu09012020 --name task1 -d --query [publicIp
 sshpass -p 'Azure.123456@e' ssh -t -t -o StrictHostKeyChecking=no azureuser@$ip << 'ENDSSH'
 sudo -S <<< "Azure.123456@e" yum update -y
 sudo -S <<< "Azure.123456@e" yum install git* -y
-#sudo -S <<< "Azure.123456@e" git clone https://github.com/kmadhukiran/Devops_new.git
+sudo -S <<< "Azure.123456@e" mkdir /var/madhu
+sudo -S <<< "Azure.123456@e" cd /var/madhu
 sudo -S <<< "Azure.123456@e" git init
+#sudo -S <<< "Azure.123456@e" git clone https://github.com/kmadhukiran/Devops_new.git
 sudo -S <<< "Azure.123456@e" git remote add origin https://github.com/kmadhukiran/Devops_new.git
 sudo -S <<< "Azure.123456@e" git remote -v
 sudo -S <<< "Azure.123456@e" git pull origin master

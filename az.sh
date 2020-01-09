c1f3426dc8c6ab2eae7d699cdf5ab05e9ba60768
kmadhukiran <58160350+kmadhukiran@users.noreply.github.com>
2020-01-10 02:50:14 +0530

    Update az.sh

--- az.sh
+++ az.sh
@@ -3,7 +3,7 @@
 #az vm create  --resource-group madhu09012020  --name  task1  --image CentOS  --admin-username azureuser  --admin-password Azure.123456@e  --location eastus
 #az vm open-port --port 80 --resource-group madhu09012020 --name task1
 #az vm open-port --port 22 --resource-group madhu09012020 --name task1
-ip=$(az vm show --resource-group madhu09012020 --name task1 -d --query [publicIps] --o tsv)
+ip=$(az vm show --resource-group madhu10012020 --name task1 -d --query [publicIps] --o tsv)
 sshpass -p 'Azure.123456@e' ssh -t -t -o StrictHostKeyChecking=no azureuser@$ip << 'ENDSSH'
 sudo -S <<< "Azure.123456@e" yum update -y
 sudo -S <<< "Azure.123456@e" yum install git* -y
@@ -15,4 +15,4 @@ sudo -S <<< "Azure.123456@e" git remote -v
 sudo -S <<< "Azure.123456@e" git pull origin master
 exit
 exit
-ENDSSH3th
+ENDSSH1

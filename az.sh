f89de6f9b5e07bed9b89e15c4bdf9421c4814e3f
kmadhukiran <58160350+kmadhukiran@users.noreply.github.com>
2020-01-09 16:39:04 +0530

    Update az.sh

--- az.sh
+++ az.sh
@@ -10,8 +10,8 @@ sudo -S <<< "Azure.123456@e" yum install git* -y
 sudo -S <<< "Azure.123456@e" mkdir /var/madhu
 sudo -S <<< "Azure.123456@e" cd /var/madhu
 #sudo -S <<< "Azure.123456@e" git clone https://github.com/kmadhukiran/Devops_new.git /var/madhu
-#sudo -S <<< "Azure.123456@e" git remote add origin https://github.com/kmadhukiran/Devops_new
-#sudo -S <<< "Azure.123456@e" git remote -v
+sudo -S <<< "Azure.123456@e" git remote add origin https://github.com/kmadhukiran/Devops_new.git
+sudo -S <<< "Azure.123456@e" git remote -v
 sudo -S <<< "Azure.123456@e" git -c /var/madhu pull
 exit
 exit

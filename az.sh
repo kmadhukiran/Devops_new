90bac721cc019ee7bb819ba5a56baf8987b7df9a
kmadhukiran <58160350+kmadhukiran@users.noreply.github.com>
2020-01-09 12:06:08 +0530

    Update az.sh

--- az.sh
+++ az.sh
@@ -1,18 +1,11 @@
-#az login -u mkancharla@nisum.com -p Changeme@123
-#az group create --name madhu123 --location eastus
-#az vm create  --resource-group madhu123  --name  myVM  --image CentOS  --admin-username azureuser  --admin-password Azure.123456@e  --location eastus
-#az vm open-port --port 80 --resource-group madhu123 --name myVM
-#az vm open-port --port 22 --resource-group madhu123 --name myVM
+az login -u mkancharla@nisum.com -p Changeme@123
+az group create --name madhu09012020 --location eastus
+az vm create  --resource-group madhu09012020  --name  task1  --image CentOS  --admin-username azureuser  --admin-password Azure.123456@e  --location eastus
+az vm open-port --port 80 --resource-group madhu09012020 --name task1
+az vm open-port --port 22 --resource-group madhu09012020 --name task1
 sshpass -p 'Azure.123456@e' ssh -t -t -o StrictHostKeyChecking=no azureuser@13.92.193.107 << 'ENDSSH'
 sudo -S <<< "Azure.123456@e" yum update -y
-sudo -S <<< "Azure.123456@e" yum install java-1.8.0-openjdk -y
-sudo -S <<< "Azure.123456@e" wget http://apachemirror.wuchna.com/tomcat/tomcat-9/v9.0.29/bin/apache-tomcat-9.0.29.tar.gz
-sudo -S <<< "Azure.123456@e" tar -xvzf apache-tomcat-9.0.29.tar.gz
-sudo -S <<< “Azure.123456@e” wget https://github.com/kmadhukiran/Devops_new/blob/master/calendar.war
-sudo -S <<< “Azure.123456@e” mv calendar.war /home/azureuser/apache-tomcat-9.0.29/webapps
-sudo -S <<< “Azure.123456@e” cd /home/azureuser/apache-tomcat-9.0.29/bin
-sudo -S <<< “Azure.123456@e” /home/azureuser/apache-tomcat-9.0.29/bin/./startup.sh
-sudo -S <<< “Azure.123456@e” echo "application deployed successfully"
+
 exit
 exit
 ENDSSH

1dbe6397aa5454f716e88d53356d29c839850376
kmadhukiran <58160350+kmadhukiran@users.noreply.github.com>
2020-01-10 12:55:30 +0530

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -1,1 +1,1 @@
-sshpass -p 'Azure.123456@e' ssh -t -t -o StrictHostKeyChecking=no azureuser@ << 'ENDSSH'
+sshpass -p 'Azure.123456@e' ssh -t -t -o StrictHostKeyChecking=no azureuser@168.61.45.159 << 'ENDSSH'
